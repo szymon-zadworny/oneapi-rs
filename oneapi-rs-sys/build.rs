deb83f5fb7bc4589baffe330519a8718f4ae55ab
Szymon Zadworny <szymon.zadworny@intel.com>
2026-06-29 14:11:14 +0000

    Use icpx compiler

--- oneapi-rs-sys/build.rs
+++ oneapi-rs-sys/build.rs
@@ -1,6 +1,10 @@
 fn main() {
+    let compiler_root = std::env::var("CMPLR_ROOT")
+        .expect("No valid OneAPI installation found.");
+
     cxx_build::bridge("src/main.rs")
-        .include("include")
+        .compiler(compiler_root + "/bin/icpx")
+        .flag("-fsycl")
         .file("src/shim.cpp")
         .std("c++17")
         .compile("test");
