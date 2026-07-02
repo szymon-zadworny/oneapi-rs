8508cfb1ba9e32efc2337c6afc0290207fb6f42b
Szymon Zadworny <szymon.zadworny@intel.com>
2026-07-02 13:55:09 +0000

    Add missing license header

--- oneapi-rs-sys/include/types.hpp
+++ oneapi-rs-sys/include/types.hpp
@@ -1,3 +1,11 @@
+//
+// Copyright (C) 2026 Intel Corporation
+//
+// Under the MIT License or the Apache License v2.0.
+// See LICENSE-MIT and LICENSE-APACHE for license information.
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+
 #pragma once
 
 #include <sycl/sycl.hpp>
