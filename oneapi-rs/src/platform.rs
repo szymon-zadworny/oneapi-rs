d5c086591c66c23043a6b9d164c3c2a7dbd06d3e
Szymon Zadworny <szymon.zadworny@intel.com>
2026-07-02 15:27:12 +0000

    Add platform documentation

--- oneapi-rs/src/platform.rs
+++ oneapi-rs/src/platform.rs
@@ -10,9 +10,16 @@ use oneapi_rs_sys::platform::ffi;
 
 use crate::{device::Device, info::platform::PlatformInfo};
 
+/// Abstraction for SYCL platform.
+///
+/// The `Platform` struct encapsulates a single SYCL platform on which SYCL kernel functions may be executed.
+/// A SYCL platform must be associated with a single SYCL backend.
+///
+/// A `Platform` is also associated with one or more SYCL devices associated with the same SYCL backend.
 pub struct Platform(pub(crate) cxx::UniquePtr<ffi::Platform>);
 
 impl Platform {
+    /// Returns a `Vec` containing all SYCL platforms from all SYCL backends available in the system.
     pub fn get_platforms() -> Vec<Self> {
         ffi::get_platforms()
             .into_iter()
@@ -20,6 +27,7 @@ impl Platform {
             .collect()
     }
 
+    /// Returns a `Vec` containing all the root devices associated with this `Platform`.
     pub fn get_devices(&self) -> Vec<Device> {
         ffi::get_devices(&self.0)
             .into_iter()
@@ -27,6 +35,9 @@ impl Platform {
             .collect()
     }
 
+    /// Queries this `Platform` for information requested by the generic parameter `Param`.
+    /// The associated type `Param::Item` must be defined in accordance with the info parameters
+    /// in `oneapi_rs::info::platform` to facilitate returning the type associated with the `Param` parameter.
     pub fn get_info<T: PlatformInfo>(&self) -> T::Item {
         T::get_item(self)
     }
